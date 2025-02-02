1dfd7c8c37a9ee959ef4b1ca4d6513ae8ef27130
Raiyan <raiyanriz11@gmail.com>
2025-02-02 17:48:41 +0400

    initial commit

---
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -0,0 +1,40 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.model.Category;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    private List<Category> categories = new ArrayList<>();
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categories;
+    }
+
+    @Override
+    public void createCategory(Category category) {
+        categories.add(category);
+    }
+
+    @Override
+    public String deleteCategory(Long categoryId) {
+
+        Category category = categories.stream()
+                .filter(c->c.getCategoryId().equals(categoryId))
+                .findFirst()
+                .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND));
+        categories.remove(category);
+
+        if(category==null){
+            return "Category not found";
+        }
+        return "Category with categoryId " +categoryId+" deleted successfully";
+    }
+}
